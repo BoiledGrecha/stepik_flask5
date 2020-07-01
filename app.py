699b8d70c114f683537abcee0d904bf64ae3b045
BoiledGrecha <av_kireev@inbox.ru>
2020-07-01 23:56:44 +0300

    index

--- app.py
+++ app.py
@@ -1,20 +1,59 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session, redirect, url_for
 import json
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from models import *
+from flask_admin import Admin
+from flask_admin.contrib.sqla import ModelView
+from random import shuffle
+import json
+
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///stepik.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.secret_key = "super_secret_random_string"
 db = SQLAlchemy(app)
-migrate = Migrate(app, db)
+migrate = Migrate(app, db) # needed?
+admin = Admin(app)
+
+admin.add_view(ModelView(User, db.session))
+admin.add_view(ModelView(Category, db.session))
+admin.add_view(ModelView(Order, db.session))
+admin.add_view(ModelView(Meal, db.session))
 
 @app.route("/")
 def first():
-        
-    return render_template("main.html")
+    categories = Category.query.all()
+    for i in categories:
+        shuffle(i.meals)
+    if not session.get("cart"):
+        cart = "Пусто"
+    else:
+        total = 0
+        tmp = json.loads(session["cart"])
+        for i in tmp.items(): 
+            total += Meal.query.filter(Meal.id == int(i[0])).first().price * int(i[1])
+        cart = "{} блюда {}руб.".format(sum(tmp.values()), total)
+            
+    return render_template("main.html", categories=categories, cart = cart)
 
+@app.route("/addtocart/<id>")
+def add_cart(id):
+    if not session.get("cart"):
+        session["cart"] = dict()
+    else:
+        session["cart"] = json.loads(session["cart"])
+    session["cart"][id] = session["cart"].get(id, 0) + 1
+    session["cart"] = json.dumps(session["cart"])
+    return redirect(url_for("cart"))
+
+#delete <-----------------
+@app.route("/kill_cart/")
+def kill_cart():
+    session.pop("cart")
+    return "OK"
+    
 @app.route("/cart/")
 def cart():
     
--- models.py
+++ models.py
@@ -1,19 +1,19 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask import Flask
 
-# app = Flask(__name__)
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///stepik.db'
-# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-# db = SQLAlchemy(app)
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///stepik.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
-db = SQLAlchemy()
+# db = SQLAlchemy()
 
 orders_meals_association = db.Table(
     "orders_meals",
     db.Column("order_id", db.Integer, db.ForeignKey("orders.id")),
     db.Column("meal_id", db.Integer, db.ForeignKey("meals.id")))
 
-class Categorie(db.Model):
+class Category(db.Model):
     __tablename__ = "categories"
     id = db.Column(db.Integer, primary_key = True)
     title = db.Column(db.String)
@@ -24,14 +24,14 @@ class Order(db.Model):
     __tablename__ = "orders"
     id = db.Column(db.Integer, primary_key = True)
     date = db.Column(db.DateTime)
-    summa = db.Column(db.Float)
+    summa = db.Column(db.Integer)
     status = db.Column(db.String)
     mail = db.Column(db.String)
     phone = db.Column(db.String)
     address = db.Column(db.String)
     
     user = db.relationship("User")
-    user_id = db.Column(db.Integer, db.ForeignKey("teachers.id"))
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
     
     meals = db.relationship("Meal", secondary=orders_meals_association,
             back_populates="orders")
@@ -41,21 +41,21 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     mail = db.Column(db.String)
     password = db.Column(db.String)
-    orders = db.relationship("Teacher")
+    orders = db.relationship("Order")
     
 
 class Meal(db.Model):
-    __tablename__ = "meales"
+    __tablename__ = "meals"
     id = db.Column(db.Integer, primary_key = True)
     title = db.Column(db.String)
-    price = db.Column(db.Float)
+    price = db.Column(db.Integer)
     description = db.Column(db.String)
     picture = db.Column(db.String)
     
-    categorie = db.relationship("Categorie")
-    categorie_id = db.Column(db.Integer, db.ForeignKey("categories.id"))
+    category = db.relationship("Category")
+    category_id = db.Column(db.Integer, db.ForeignKey("categories.id"))
     
     orders = db.relationship("Order", secondary=orders_meals_association,
             back_populates="meals")
 
-# db.create_all()
+db.create_all()
